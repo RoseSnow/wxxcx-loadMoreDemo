1ee00ea80b08ee1656c0ff1246fea0c942199354
RoseSnow <respect_69@183.com>
2019-01-15 15:06:48 +0800

    增加了判断是否加载到底

--- pages/imgs/imgs.js
+++ pages/imgs/imgs.js
@@ -1,4 +1,3 @@
-//index.js
 //获取应用实例
 const app = getApp()
 
@@ -7,7 +6,7 @@ Page({
     hidden: true,
     windowHeight: 0,
     list:[],
-    page: 3
+    page: 1
   },
 
   // API接口：https://www.apiopen.top/meituApi?page=3
@@ -15,56 +14,74 @@ Page({
   onLoad(){
       // 获取屏幕高度
     wx.getSystemInfo({
-      success: (res) => {
+      success: res => {
         this.setData({
           windowHeight: res.windowHeight
         })
-        console.log(res.windowHeight);
+        // console.log(res.windowHeight);
       }
     });
-    this.getImages()
+
+    // 调用getImages
+    this.getImages();
   },
 
   // 请求数据
+  // api: https://www.apiopen.top/meituApi?
   getImages(){
-    const apiUrl = "https://www.apiopen.top/meituApi?" + this.data.page;
+    // 模板字符串
+    const apiUrl=`https://www.easy-mock.com/mock/5c36b5d2be0a1c39bfd57589/img/page=${this.data.page}`
+    
+    // const apiUrl = "https://www.apiopen.top/meituApi?page=" + this.data.page;
     wx.request({
-        url: apiUrl,
-        data:{
-        },
-        header:{
-            'content-type': 'json'
-        },
-        success: (res)=>{
-            this.setData({
-            // 追加列表
-            list: this.data.list.concat(res.data.data)
-            })
-        }
+      url: apiUrl,
+      data:{
+      },
+      header:{
+          'content-type': 'json'
+      },
+      success: res => {
+        // console.log(res.data);
+        // console.log(res.data.data.length);
+          this.setData({
+          // 把请求的数据存放到 data中的list中
+          // list: res.data.data
+          // 追加列表
+          list: this.data.list.concat(res.data.data),
+          })
+      }
     })
   },
   
-  // 上划加载更多
+  //上划加载更多
   loadMore(){
-    //console.log(this.data.page);
-    this.setData({
+    console.log(this.data.page);
+    // console.log(this.data.listLength);
+    if(this.data.page < 2){
+      this.setData({
         page: this.data.page + 1,
         // 显示加载动画
         hidden: false
-    });
-    
-    // 持续1.5秒的加载动画，并执行加载操作
-    setTimeout( () => {
-        this.setData({
-            hidden: true  //隐藏加载动画
+      });
+      
+      // 持续1.5秒的加载动画，并执行加载操作
+      setTimeout( () => {
+          this.setData({
+              hidden: true  //隐藏加载动画
+          })
+          this.getImages()
+      },1500);
+    }else{
+      wx.showLoading({
+        title: "玩儿命加载中..."
+      })
+      setTimeout(() => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '已经到底啦!'
         })
-        this.getImages()
-    },1500)
+      }, 1000)
       
-  },
-
-  updateMore() {
-      
-  },
-  
+    }
+  }
 })
